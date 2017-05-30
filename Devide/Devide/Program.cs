c819043915d44443a9654ad0dcdf275db4475c84
nalbandyan.1991 <nalbandyan.1991@bk.ru>
2017-05-31 00:26:50 +0400

    Some errors Devide

---
+++ Devide/Devide/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N=");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("M=");
+            int m = int.Parse(Console.ReadLine());
+            Devides(n, m);
+        }
+        public static void Devides(int n,int m,int i=2)
+        {
+            
+            while ((n % i == 0) && (m % i == 0)  )
+                if ((i <= n / 2) && (i <= m / 2))
+                {
+                    Console.WriteLine(i);
+                    ++i;
+                    Devides(n, m,i++);
+                }
+                else
+                 {
+                 Devides(n, m,i++);++i;
+                 }
+            
+
+        }
+    }
+}
---
+++ MaxElement/MaxElement/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            while (true)
+            {
+                Console.Write("N=");
+                int n = int.Parse(Console.ReadLine());
+                List<int> A = new List<int>();
+                for (int i = 0; i < n; i++)
+                {
+                    A.Add(n - i);
+                    Console.Write(A[i]+"\t");
+                }
+                List<int> B = new List<int>();
+                    
+            }
+            
+        }
+        static void ShowList(List<int> list)
+        {
+            if (List count==0)
+                Console.WriteLine(  "Empty");
+            foreach (int i in list)
+            {
+                Console.WriteLine(i+"\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
