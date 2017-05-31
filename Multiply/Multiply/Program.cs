9101c7a3e54985cc5075d3732f83420713ba1e3b
nalbandyan.1991 <nalbandyan.1991@bk.ru>
2017-05-31 16:49:08 +0400

    Full Level

---
+++ Multiply/Multiply/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N=");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("M=");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine(Multy(n,m));
+        }
+        static int Multy(int n,int m)
+        {
+
+            if (n> 0)
+            {
+                return (m + Multy(n-1, m));
+                
+            }
+            return 0;
+           
+        }
+
+    }
+}
